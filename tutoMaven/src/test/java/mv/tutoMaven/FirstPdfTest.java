9eb14c54eb1d5c79f5c0101100109dbffdf22954
julie <julie@LAPTOP-GLAHQSQM.home>
2021-01-29 15:30:50 +0100

    COMMIT DE BASE

---
+++ tutoMaven/src/test/java/mv/tutoMaven/FirstPdfTest.java
@@ -0,0 +1,28 @@
+package mv.tutoMaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.itextpdf.text.Document;
+
+public class FirstPdfTest {
+
+    @Test
+    public void addMetaDataTest()
+    {/**
+    	Document document1 = new Document();
+    	FirstPdf.addMetaData(document1);
+    	Document document2 = new Document();
+    	document2.addTitle("My first PDF");
+    	document2.addTitle("My first PDF");
+        document2.addSubject("Using iText");
+        document2.addKeywords("Java, PDF, iText");
+        document2.addAuthor("Olivier Barais");
+        document2.addCreator("Olivier Barais");
+        assertEquals(document1,document2);
+    }
+**/
+    assertTrue(true);
+}
+}
