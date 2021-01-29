7b4d59c4960f659b5201e0f53572dcd7d8fa284f
julie <julie@LAPTOP-GLAHQSQM.home>
2021-01-29 15:12:35 +0100

    commit projet IDOM

---
+++ tutoMaven/src/main/java/mv/tutoMaven/App.java
@@ -0,0 +1,13 @@
+package mv.tutoMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+    }
+}
